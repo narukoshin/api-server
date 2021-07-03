344376a89423f2e97ae57b4565cf94d923ae67a0
yuuhirokabe <alekss273@inbox.lv>
2021-07-03 22:47:31 +0300

    added hatty comment

--- server/src/main.go
+++ server/src/main.go
@@ -1,6 +1,9 @@
 package main
 
 /**
+
+	 "package main" is like a hat for my code, so, that's nice, I hope that hat is black.
+
 		Author of this fucking website is fucking motherfucker who loves to broke things... idk if this shit will ever run.
 	Compile errors - 1425, seems to there's no problems.
 		I'm Naru Koshin, be afraid from me and my code because your eyes will burn when you will read my code and of course, you will lost your virginity.

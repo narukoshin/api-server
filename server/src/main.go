5b4c800b303a17b67fe44f5d3efc0f39b3f9a3e1
yuuhirokabe <alekss273@inbox.lv>
2021-07-03 22:42:30 +0300

    first commit

---
+++ server/src/main.go
@@ -0,0 +1,13 @@
+package main
+
+/**
+		Author of this fucking website is fucking motherfucker who loves to broke things... idk if this shit will ever run.
+	Compile errors - 1425, seems to there's no problems.
+		I'm Naru Koshin, be afraid from me and my code because your eyes will burn when you will read my code and of course, you will lost your virginity.
+**/
+
+func main(){
+	// my fucking awesome website.
+
+	// sorry, there's no code to read... please come back later.
+}
